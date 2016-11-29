5fd6c5383f01b90c9dd04d0f6696d15d64b3bc3b
Cory <cory.forsyth@gmail.com>
2016-11-29 10:42:01 -0700

    Prefix version in downloaded schema filename with 'v'

--- app/models/schema.js
+++ app/models/schema.js
@@ -1,11 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
 const FILE_TYPE = 'application/json;charset=utf-8';
 
 export default DS.Model.extend({
   schema: DS.attr(),
   fileName: Ember.computed('schema.id', function() {
     let segments = this.get('schema.id').split('/').reverse();
-    return `${segments[1]}_${segments[0]}.json`;
+    return `${segments[1]}_v${segments[0]}.json`;
   }),
 
   export() {
